cee5f3e9cc228cf0727b7e95a48ef3d448c182a5
Larry <melarry46@gmail.com>
2025-07-21 09:40:51 +0300

    done

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,11 @@ const app = express();
 
 app.use(cors());
 
-// Endpoint to serve produce config
 app.get('/api/produce-config', (req, res) => {
   const config = require(path.join(__dirname, 'produceConfig.json'));
   res.json(config);
 });
 
-// Default port or 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend server running on port ${PORT}`);
